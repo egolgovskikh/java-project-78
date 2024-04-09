aff8f86263dd0b94ada8237eb4a33daf3e2105e0
egolgovskikh <egolgovskikh@gmail.com>
2024-04-09 07:55:01 +0300

    add integer validator

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -1,17 +1,32 @@
 package hexlet.code;
 
+import hexlet.code.schemas.NumberSchema;
 import hexlet.code.schemas.StringSchema;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ValidatorTest {
 
+    private Validator validator;
+
+    @BeforeEach
+    void beforeEach() {
+        validator = new Validator();
+    }
+
     @Test
     void testString() {
-        Validator v = new Validator();
-        StringSchema actual = v.string();
+        StringSchema actual = validator.string();
         StringSchema expected = new StringSchema();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void number() {
+        NumberSchema actual = validator.number();
+        NumberSchema expected = new NumberSchema();
         assertEquals(expected, actual);
     }
 }
